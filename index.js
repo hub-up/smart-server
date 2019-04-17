56abbf645d2f39de09dcaf33341c10cf0899b902
Joseph <z@charmed.tech>
2019-04-17 14:55:19 -0700

    Start mongoose database on login

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 'use strict';
 
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3000;
 const { start } = require('./src/server.js');
+
+// Start up database server with options
+const mongoose = require('mongoose');
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+};
+
+mongoose.connect(process.env.MONGODB_URI, options, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Mongoose connected...`);
+  }
+});
 
 start(PORT);
