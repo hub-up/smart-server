2efa9d99492d8606b8bd15b4b2b8f959b13ff2d9
Joseph <z@charmed.tech>
2019-04-18 08:44:54 -0700

    Adjust about function for server-side

--- src/api/command-handlers/about.js
+++ src/api/command-handlers/about.js
@@ -1,9 +1,16 @@
 'use strict';
 
-const log = require('../log.js');
-// const chalk = require('chalk');
+const sendToUser = require('../lib/send-to-user.js');
 
-const about = () => {
+/***
+ * See information about the project and development team
+ * @function
+ * @name fallback
+ * @param undefined {undefined} Unused parameter
+ * @param socket {object} The socket object from the client event
+ * @param io {object} The server-side Socket.io instance
+ */
+const about = (undefined, socket, io) => {
   const message = `
 ==================================
 _        _    _         _    
@@ -33,7 +40,7 @@ Hirata, and Aaron Bruce
 
 ===================================
 `;
-  log(message);
+  sendToUser(message, socket, io);
 };
 
 module.exports = about;
