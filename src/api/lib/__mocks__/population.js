4238a570a85c8470b0a4b779e6c53717894a03f7
Joseph <z@charmed.tech>
2019-04-21 11:34:32 -0700

    Create additional users and put them in mock rooms

--- src/api/lib/__mocks__/population.js
+++ src/api/lib/__mocks__/population.js
@@ -15,8 +15,27 @@ const username2 = faker.lorem.word();
 const socketId3 = faker.random.number();
 const username3 = faker.lorem.word();
 
+const room1 = faker.lorem.word();
+
 population.addUser(socketId1, username1);
 population.addUser(socketId2, username2);
 population.addUser(socketId3, username3);
+
+population.populateRoom(socketId1, room1);
+population.populateRoom(socketId2, room1);
+population.populateRoom(socketId3, room1);
+
+// A hardcoded version for querying details
+const socketId4 = 1;
+const username4 = 'Bob';
+const room2 = 'lobby';
+population.addUser(socketId4, username4);
+population.populateRoom(socketId4, room2);
+
+const socketId5 = 2;
+const username5 = 'Jane';
+const room3 = 'special';
+population.addUser(socketId5, username5);
+population.populateRoom(socketId4, room3);
 
 module.exports = population;
