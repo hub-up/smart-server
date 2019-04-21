0d243869acbc238bc51f384a3ecf0f994479ceb4
Joseph <z@charmed.tech>
2019-04-21 11:34:03 -0700

    Note for updates

--- src/api/lib/__mocks__/send-to-room.js
+++ src/api/lib/__mocks__/send-to-room.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const sendToRoom = (message, room, socket) => {
-  const args = {message, room, socket};
+  const args = { message, room, socket };
   args;
 };
 
+// Need to rewire to make this export and object like the real one
 module.exports = sendToRoom;
