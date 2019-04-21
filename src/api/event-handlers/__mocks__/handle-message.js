ff30434695f483ff4fa30fa5c2013e5e4c377ac2
Joseph <z@charmed.tech>
2019-04-21 10:13:15 -0700

    Add handle-message mock

---
+++ src/api/event-handlers/__mocks__/handle-message.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mockSocket = {id:Math.random(), to: jest.fn(), emit: jest.fn()};
+
+const population = require('../../lib/population.js');
+const sendToRoom = require('../../lib/send-to-room.js');
+
+const handleMessage = (line, socket = mockSocket) => {
+  const username = population.getUsername(socket.id);
+  const room = population.getRoom(socket.id);
+
+  sendToRoom(line, room, socket);
+};
+
+module.exports = handleMessage;
