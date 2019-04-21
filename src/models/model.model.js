a06dec0057f55700fc3eb54dbcf555688a942b27
Joseph <z@charmed.tech>
2019-04-21 13:13:15 -0700

    Revert to using `find` on get(name)
    client expects an array

--- src/models/model.model.js
+++ src/models/model.model.js
@@ -9,7 +9,7 @@ class ModelModel {
   }
 
   get(name) {
-    return name ? this.schema.findOne({ name }) : this.schema.find();
+    return name ? this.schema.find({ name }) : this.schema.find();
   }
 
   post(obj) {
