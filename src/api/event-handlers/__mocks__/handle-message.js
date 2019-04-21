0ba026bb9f2e249d5e4dc20a14dab2dc8c76a647
Joseph <z@charmed.tech>
2019-04-21 13:11:20 -0700

    Update import of sendToRoom; 'use' username

--- src/api/event-handlers/__mocks__/handle-message.js
+++ src/api/event-handlers/__mocks__/handle-message.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const mockSocket = {id:Math.random(), to: jest.fn(), emit: jest.fn()};
+const mockSocket = { id: Math.random(), to: jest.fn(), emit: jest.fn() };
 
 const population = require('../../lib/population.js');
-const sendToRoom = require('../../lib/send-to-room.js');
+const { sendToRoom } = require('../../lib/send-to-room.js');
 
 const handleMessage = (line, socket = mockSocket) => {
   const username = population.getUsername(socket.id);
+  username;
   const room = population.getRoom(socket.id);
 
   sendToRoom(line, room, socket);
